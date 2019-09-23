c9e5ce92fa285647a0255a8fb5e509c35c7c93d1
Simon Maher <simon.maher@integrify.io>
2019-09-23 12:44:06 +0300

    created app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import CardArray from './cardArray'
+import Scroll from './scroll'
+import SearchBox from './searchBox'
+import './index.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Creating the parent App Component. Because we are using state, we use class, constructor and super so that we can setState and use props  
+class App extends Component {
+    constructor(){
+        super()
+        this.state= {
+            robots:[],
+            searchfield:''
+        }
+    }
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users').then(response => {
+           return  response.json()
+        })
+            .then (users => {
+                this.setState({robots:users})
+        })
+    }
+    onSearchChange = (event) => {
+        this.setState({searchfield:event.target.value})
+    }
+    render (){
+      const filteredRobots = this.state.robots.filter(robot=>{
+      return robot.name.toLowerCase().includes(this.state.searchfield.toLocaleLowerCase())
+     })
+     if(this.state.robots.length === 0) {
+         return <h1>Loading</h1>
+     }else {
+
+         return (
+             <div className="tc">
+    <h1 className="f1">RoboFriends</h1>
+ <SearchBox searchChange={this.onSearchChange} />
+ <Scroll>
+
+ <CardArray robots = {filteredRobots}/>
+ </Scroll>
+ </div>
+ )
 }
+  }
+ }
+ export default App
+        
 
-export default App;
+        
+
+
+        
+
+
+
+        
+        
+        
